e94b7b6b1b7d7824d474e86d8064b6915d515f54
bogdan27-shr <bolaiserezam@gmail.com>
2026-06-09 16:12:02 +0200

    new validation

--- script/script.js
+++ script/script.js
@@ -5,7 +5,7 @@ function createTags(
   className = undefined,
   value = undefined,
   id = undefined
-) {
+){
   let tag = document.createElement(tagName);
 
   if (className) {
@@ -26,6 +26,10 @@ function createTags(
     return tag;
   }
 }
+let modalDiv = createTags(`div`, `modal`);
+let modalSpan = createTags(`span`);
+modalDiv.append(modalSpan);
+
 window.addEventListener(`DOMContentLoaded`, () => {
   const contacts = document.querySelector(`.contacts`);
 
@@ -64,6 +68,49 @@ window.addEventListener(`DOMContentLoaded`, () => {
       menu.classList.remove(`show`);
       menu.classList.add(`hide`);
       modeMenu = `hide`;
+    }
+  });
+
+  const btnUser = document.querySelector(`#btnSend`);
+  btnUser.addEventListener(`click`, (e) => {
+    const validName = /[а-я a-z]{2,15}/i;
+    const validEmail = /^[a-z]{2,15}@[a-z]{2,8}\.[a-z]{2,8}(\.[a-z]{2,8})?$/i;
+
+    let userName = document.querySelector(`#userName`);
+    let userEmail = document.querySelector(`#userEmail`);
+    let userMesagge = document.querySelector(`#userMessage`);
+    if(validName.test(userName.value) && validEmail.test(userEmail.value)){
+      let form = document.querySelector(`.contForForm .form`);
+
+      modalSpan.textContent = `The data has been sent`;
+      form.prepend(modalDiv);
+      setTimeout(() => {
+        modalDiv.classList.add(`ok`);
+        setTimeout(() => {
+          modalDiv.classList.add(`delete`);
+          modalDiv.remove();
+          setTimeout(() => {
+            modalDiv.classList.remove(`ok`);
+            modalDiv.classList.remove(`delete`);
+          }, 2100);
+        }, 2000);
+      }, 1);
+    }else{
+      let form = document.querySelector(`.contForForm .form`);
+
+      modalSpan.textContent = `Contacts are incorrect`;
+      form.prepend(modalDiv);
+      setTimeout(() => {
+        modalDiv.classList.add(`error`);
+        setTimeout(() => {
+          modalDiv.classList.add(`delete`);
+          modalDiv.remove();
+          setTimeout(() => {
+            modalDiv.classList.remove(`error`);
+            modalDiv.classList.remove(`delete`);
+          }, 2100);
+        }, 2000);
+      }, 1);
     }
   });
 });
